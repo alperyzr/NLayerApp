d1057ed6e98c24517b13dbecad98c4e5540c3664
Alper Yazır <alperyzrr@gmail.com>
2022-07-20 00:30:42 +0300

    appsetting db yolu belirtildi. API projesinin program.cs inde appseetingsten okuması sağlandı. İlk migratiın atılıp Db oluşturuldu

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,3 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository;
+using NLayer.Repository.Repositories;
+using NLayer.Repository.UnitOfWork;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//======= INTERFACE IMPLEMANTASYONLARI
+builder.Services.AddScoped<IUnitOfWorkService, UnitOfWork>();
+//generic olduðu için typeof kullanarak bu þekilde yazdýk.
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+
+
+//Db baðlantýsý için appsettingste ki ConnectionString verilir
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    //appsettingsten okuyacaðý kýsým
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+     {
+         //Dinamic olarak AppDbContext classýný içinde barýndýran Repository projesinin ismini belirtmiþ oluyoruz
+         option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+     });
+});
+
 
 var app = builder.Build();
 
