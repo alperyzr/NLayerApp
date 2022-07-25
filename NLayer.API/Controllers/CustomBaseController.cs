e76189f85ecf245bee1a939113c0ceb13e7c95cb
Alper Yazır <alperyzrr@gmail.com>
2022-07-26 00:48:07 +0300

    API ProductController methodları yazdıldı

---
+++ NLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        //API Controllerda rreturn OK veya BadRequest Demek yerine generic bir method yazıldı
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomResponseDTO<T> response)
+        {
+            if (response.StatusCode == 204)
+            {
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+            }
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
---
+++ NLayer.API/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Entities;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    public class ProductController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductController(
+            IMapper mapper,
+            IService<Product> service)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _service.GetAllAsync();
+            var productsDto = _mapper.Map<List<ProductDto>>(products.ToList());
+            return CreateActionResult(CustomResponseDTO<List<ProductDto>>.Success(200, productsDto));
+        }
+
+        //www.mysite.com/api/products/5 anlamında kullanılır
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById(int Id)
+        {
+            var product = await _service.GetByIdAsync(Id);
+            var productDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomResponseDTO<ProductDto>.Success(200, productDto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDto productDto)
+        {
+            var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            var responseDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomResponseDTO<ProductDto>.Success(201, responseDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDto));
+            return CreateActionResult(CustomResponseDTO<NoContentDto>.Success(204));
+        }
+
+        //www.mysite.com/api/products/5
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Remove(int Id)
+        {
+            var product = await _service.GetByIdAsync(Id);
+            await _service.RemoveAync(product);          
+            return CreateActionResult(CustomResponseDTO<NoContentDto>.Success(204));
+        }
+    }
+}
